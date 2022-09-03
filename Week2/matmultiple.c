2985331dd5442589c1f0641e10530d414f22fde6
Punith <110713318+punithnayak@users.noreply.github.com>
2022-09-03 16:45:16 +0530

    Update matmultiple.c

--- Week2/matmultiple.c
+++ Week2/matmultiple.c
@@ -1,99 +1,88 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void Prod(int **a, int **b, int **r, int m1, int m2, int n2){
-    int sum=0;
-    for (int i = 0; i < m1; i++)
-    {
-        for (int j = 0; j < n2; j++)
-        {
-            for (int k = 0; k < m2; k++)
-            {
-                sum += a[i][k] * b[k][j];             
-            }
-
-            r[i][j] = sum;
-            sum = 0;
-        }
-    }
-    printf("Product of matrix is: \n");
-
-    for (int i = 0; i < m1; i++)
-    {
-        for (int j = 0; j < n2; j++)
-        {
-           printf("%d ", r[i][j]); 
-        }
-        printf("\n");     
-    }   
-}
-
-
-int **allocate(int m, int n){
-    int **matrix;
-    matrix = (int **) calloc(m, sizeof(int *));
-    for (int i = 0; i < n; i++)
-    {
-        *(matrix+i) = (int *) calloc(n, sizeof(int));
-    }
-    return matrix;
-}
-
-int read(int **matrix, int m, int n){
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            scanf("%d", matrix[i][j]);
-        }   
-    }
-    
-}
-int print(int **matrix, int m, int n){
-    printf("\n Inside print \n");
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            printf("%d ", matrix[i][j]);
-        }
-        printf("\n");   
-    }
-}
-
-int main(int argc, char const *argv[])
+void product(int **a,int **b,int **r,int r1,int c1,int c2)
 {
-    int n1, m1, n2, m2;
-    int **A, **B, **R;
-    printf("Enter row of matrix 1: ");
-    scanf("%d", &m1);  
-    printf("Enter column of matrix 1: ");
-    scanf("%d", &n1);  
-    printf("Enter row of matrix 2: ");
-    scanf("%d", &m2); 
-    printf("Enter column of matrix 2: ");
-    scanf("%d", &n2);
+ for(int i=0;i<r1;i++){
+  
+  	for(int j=0;j<c2;j++){
+  	   r[i][j]=0;
+  		for(int k=0;k<c1;k++){
+  		
+          r[i][j]+=a[i][k]*b[k][j];
+  		}
+  	}
+ }
+ printf("New matrix is \n");
+ for(int i=0;i<r1;i++)
+  {
+  	for(int j=0;j<c2;j++)
+  	{
+  		printf("\t %d",r[i][j]);
+  	}
+  	printf("\n");
+  }
+}
 
-    A = allocate(m1, n1);
-    printf("Enter the matrix : ");
-    read(A, m1, n1);  
-    printf("Entered matrix is: \n");
-    print(A, m1, n1);
-    B = allocate(m2, n2);
-    printf("Enter the matrix : ");
-    read(B, m2, n2);
-    printf("Entered matrix is: \n");
-    print(B, m2, n2);
-
-    if(n1 == m2)
-    {
-        R = allocate(m1, n2);
-        Prod(A, B, R, m1, m2, n2);
-    }
-    else{
-        printf("Cannot multiply\n");
-    }
+int **allocate(int r1, int c1)
+{
+int i;
+int **matrix;
+matrix=(int **)calloc(r1, sizeof(int *));
+for(i=0;i<c1;i++)
+*(matrix+i)=(int *)calloc(c1,sizeof(int));
+return matrix;
+}
 
 
-    return 0;
+int read(int **matrix, int r1, int c1)
+{
+
+	for(int i=0;i<r1;i++)
+	{
+		for(int j=0;j<c1;j++)
+		{
+			scanf("%d",(*(matrix+i)+j));
+		}
+	}
+}
+
+int print(int **matrix, int r1, int c1)
+{
+
+	for(int i=0;i<r1;i++)
+	{
+		for(int j=0;j<c1;j++)
+		{
+			printf("%d",*(*(matrix+i)+j));
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+int r1,c1,r2,c2;
+int **A,**B,**R;
+        printf("Enter dimensions of Matrix 1:\n");
+	scanf("%d %d",&r1, &c1);
+	A=allocate(r1,c1);
+	printf("Enter dimensions of Matrix 2:\n");
+	scanf("%d %d",&r2, &c2);
+	B=allocate(r2,c2);
+	if(c1==r2)
+	{
+	printf("Enter the elements of Matrix 1:\n");
+	read(A,r1,c1);
+	printf("The matrix is:");
+	print(A,r1,c1);
+	printf("Enter the elements of Matrix 2:\n");
+	read(B,r1,c1);
+	printf("The matrix is:");
+	print(B,r2,c2);
+	R=allocate(r2,c2);
+	product(A,B,R,r1,c1,c2);
+	}
+	else
+	printf("Wrong dimensions");	
 }
